850c6a75fd46c484915130e88bc361466eefd3d5
shivam7086 <161806939+shivam7086@users.noreply.github.com>
2024-05-09 16:26:03 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -6,7 +6,7 @@
 `default_nettype none
 
 module tt_um_example (
-    input  wire [4:0] ui_in,    // Dedicated inputs
+    input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
     output wire [7:0] uio_out,  // IOs: Output path
