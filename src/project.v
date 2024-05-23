d61beae81eb4bd242dfef48d237af4c5864819fb
shivam7086 <161806939+shivam7086@users.noreply.github.com>
2024-05-24 00:56:49 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -17,7 +17,7 @@ module tt_um_PUF (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out1  = uo_out + uio_out;  // Example: ou_out is the sum of ui_in and uio_in
+  assign uo_out  = uo_out + uio_out;  // Example: ou_out is the sum of ui_in and uio_in
   // assign uio_out = 0;
   assign uio_oe  = 0;
 //module Top (output [7:0] ui_out, input ena, rst_n, input [4:0] ui_in);
