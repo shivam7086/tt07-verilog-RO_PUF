7973d97f74b203de478be5a7562c830604af0a06
shivam7086 <161806939+shivam7086@users.noreply.github.com>
2024-05-26 02:16:33 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -20,10 +20,10 @@ module tt_um_PUF (
       //assign uo_out = uo_out + uio_out;
     //assign ui_in= ui_in + uio_in; // Example: ou_out is the sum of ui_in and uio_in
   // assign uio_out = 0;
-    output reg [15:0] u_out;
-    u_out = uo_out + uio_out;
-    input reg [15:0] u_in;
-    u_in =  ui_in + uio_in;
+    // output reg [15:0] u_out;
+    // u_out = uo_out + uio_out;
+    // input reg [15:0] u_in;
+    // u_in =  ui_in + uio_in;
   assign  uio_out=0;
   assign uio_oe = 0;
 //module Top (output [7:0] ui_out, input ena, rst_n, input [4:0] ui_in);
