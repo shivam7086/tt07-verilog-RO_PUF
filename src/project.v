8970316462eef2eee8d2dacac6d5651aea8d08ae
shivam7086 <161806939+shivam7086@users.noreply.github.com>
2024-05-28 09:21:08 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -55,7 +55,7 @@ f2g g4(p[4], q[4], r[4], q[3], 1'b1, 1'b1);
 assign A=out;
 endmodule
 
-module top_f2g (output [15:0] count, input ena, rst_n, input [4:0] Uin); // make change input [4:0]
+module top_f2g (output [15:0] count, input ena, rst_n, input [3:0] Uin); // make change input [4:0]
     wire [15:0] i;
 wire mux_out;
 genvar x;     
