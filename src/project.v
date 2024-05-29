2eaa432f2f50a4bdfc7c90c4efe9a36c935a5871
shivam7086 <161806939+shivam7086@users.noreply.github.com>
2024-05-29 18:42:02 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -13,7 +13,9 @@ module tt_um_PUF (
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
     input  wire       ena,      // always 1 when the design is powered, so you can ignore it
     input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+    input  wire       rst_n,    // reset_n - low to reset
+    input wire   [15:0] Uin,
+    output wire  [15:0] Uout
 );
 
   // All output pins must be assigned. If not used, assign to 0.
